18052852a66bfd199df50767ebd2012aaaeea3cd
Alejandro Poveda <30927845+seapovedac@users.noreply.github.com>
2022-08-11 12:57:14 +0200

    Update 1.pca_apo_and_holo.sh

--- pca/1.pca_apo_and_holo.sh
+++ pca/1.pca_apo_and_holo.sh
@@ -5,7 +5,7 @@ trajectory=trajUG_no-mem_trajBR_no-mem_and_trajUG_yes-mem_trajBR_yes.xtc
 
 gmx covar -s $reference -f $trajectory -xpm
 
-echo -e 1 1 | gmx anaeig -v eigenvec.trr -f $trajectory -s $reference -eig eigenval.xvg -comp -proj -first 1 -last 2 -2d
+echo 1 1 | gmx anaeig -v eigenvec.trr -f $trajectory -s $reference -eig eigenval.xvg -comp -proj -first 1 -last 2 -2d
 
 for ((i=1; i<=3 ; i+=1)) do
 
@@ -30,25 +30,25 @@ for ((i=1; i<=3 ; i+=1)) do
 
 	mkdir ug_without_mem
 	cd ug_without_mem
-	echo -e 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_UG.xvg -b 0 -e 27 -tu us
+	echo 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_UG.xvg -b 0 -e 27 -tu us
 	egrep -v "\#|@|\&" 2dproj_BB_UG.xvg > data
 	cd ..
 
 	mkdir br_without_mem
 	cd br_without_mem
-	echo -e 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_BR.xvg -b 27 -e 54 -tu us
+	echo 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_BR.xvg -b 27 -e 54 -tu us
 	egrep -v "\#|@|\&" 2dproj_BB_BR.xvg > data
 	cd ..
 
 	mkdir ug_with_mem
 	cd ug_with_mem
-	echo -e 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_UG.xvg -b 54 -e 111 -tu us
+	echo 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_UG.xvg -b 54 -e 111 -tu us
 	egrep -v "\#|@|\&" 2dproj_BB_UG.xvg > data
 	cd ..
 
 	mkdir br_with_mem
 	cd br_with_mem
-	echo -e 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_BR.xvg -b 111 -e 168 -tu us
+	echo 1 1 | gmx anaeig -v ../../eigenvec.trr -f ../../$trajectory -s ../../$reference -eig ../../eigenval.xvg -comp -proj -first ${first_pc} -last ${second_pc} -2d 2dproj_BB_BR.xvg -b 111 -e 168 -tu us
 	egrep -v "\#|@|\&" 2dproj_BB_BR.xvg > data
 	cd ..
 	
